f168cc9f2e2332ac1c49f37e84de27f3573a4991
Amritpal Singh <amritpal.singh@honeywell.com>
2022-06-21 14:46:10 +0530

    pinch app completion

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -12,18 +12,27 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
+    @State private var isDrawerOpen: Bool = false
 
+    let pages: [Page] = pageData
+    @State private var pageIndex: Int = 1
+    
     func resetImageState() {
-        withAnimation(.linear) {
+        withAnimation(.spring()) {
             imageScale = 1
             imageOffset = .zero
         }
     }
     
+    func currentPage() -> String {
+        return pages[pageIndex - 1].imageName
+    }
+    
     var body: some View {
         NavigationView{
             ZStack {
-                Image("magazine-front-cover")
+                Color.clear
+                Image(currentPage())
                     .resizable()
                     .scaledToFit()
                     .cornerRadius(10)
@@ -45,12 +54,33 @@ struct ContentView: View {
                     .gesture(
                         DragGesture()
                             .onChanged({ value in
-                                withAnimation(.linear) {
+                                withAnimation(.spring()) {
                                     imageOffset = value.translation
                                 }
                             })
                             .onEnded({ _ in
                                 if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
+                    .gesture(
+                        MagnificationGesture()
+                            .onChanged({ value in
+                                print(value)
+                                withAnimation(.linear(duration: 1)) {
+                                    if value >= 0.5 && value <= 5 {
+                                        imageScale = value
+                                    } else if value > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            })
+                            .onEnded({ value in
+                                print("vv: \(value)")
+                                if imageScale > 5 {
+                                    imageScale = 5
+                                } else if imageScale <= 1 {
                                     resetImageState()
                                 }
                             })
@@ -61,6 +91,93 @@ struct ContentView: View {
             .onAppear {
                 isAnimating = true
             }
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                ,alignment: .top
+            )
+            .overlay(
+                Group {
+                    HStack {
+                        Button {
+                            withAnimation(.spring()) {
+                                if imageScale > 1 {
+                                    imageScale -= 1
+                                    if imageScale <= 1 {
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "minus.magnifyingglass")
+                        }
+                        
+                        Button {
+                            resetImageState()
+                        } label: {
+                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+                        
+                        Button {
+                            withAnimation(.spring()) {
+                                if imageScale < 5 {
+                                    imageScale += 1
+                                    if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "plus.magnifyingglass")
+                        }
+                    }
+                    .padding(EdgeInsets.init(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                }
+                    .padding(.bottom, 30)
+                ,alignment: .bottom
+            )
+            .overlay(
+                
+                HStack {
+                    Image(systemName: isDrawerOpen ? "chevron.compact.right" : "chevron.compact.left")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 40)
+                        .padding(8)
+                        .foregroundStyle(.secondary)
+                        .onTapGesture {
+                            withAnimation {
+                                isDrawerOpen.toggle()
+                            }
+                        }
+                    ForEach(pages) { item in
+                        Image(item.thumbnail)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(8)
+                            .shadow(radius: 4)
+                            .onTapGesture {
+                                isAnimating = true
+                                pageIndex = item.id
+                            }
+                    }
+                    .opacity(isDrawerOpen ? 1 : 0)
+                    .animation(.linear(duration: 0.3), value: isDrawerOpen)
+                    Spacer()
+                }
+                    .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                    .frame(width: 260)
+                    .padding(.top, UIScreen.main.bounds.height / 12)
+                    .offset(x: isDrawerOpen ? 0 : 215)
+                ,alignment: .topTrailing
+            )
         }
         .navigationViewStyle(.stack)
     }
